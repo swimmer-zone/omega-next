3b90b6311254d063b3d91bf5caf9f4688612ade2
Swimmer <11476014+swimmer-zone@users.noreply.github.com>
2025-08-25 15:13:10 +0200

    Add logos

---
+++ src/Components/_scss/index.js
@@ -0,0 +1,21 @@
+import carousel from './carousel.scss';
+import code from './code.scss';
+import footer from './footer.scss';
+import hexagons from './hexagons.scss';
+import logo from './logo.scss';
+import menu from './menu.scss';
+import music from './music.scss';
+import rating from './rating.scss';
+import social from './social.scss';
+
+export {
+    carousel,
+    code,
+    footer,
+    hexagons,
+    logo,
+    menu,
+    music,
+    rating,
+    social
+};
---
+++ src/pages/_scss/index.js
@@ -0,0 +1,21 @@
+import page from './_page.scss';
+import variables from './_variables.scss';
+import footer from './footer.scss';
+import hexagons from './hexagons.scss';
+import logo from './logo.scss';
+import menu from './menu.scss';
+import music from './music.scss';
+import rating from './rating.scss';
+import social from './social.scss';
+
+export {
+    page,
+    variables,
+    footer,
+    hexagons,
+    logo,
+    menu,
+    music,
+    rating,
+    social
+};
