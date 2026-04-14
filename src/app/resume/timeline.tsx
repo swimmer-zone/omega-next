3b7653574b8ef3446ab6ef5993d19c434b67ff46
Swimmer <11476014+swimmer-zone@users.noreply.github.com>
2026-04-14 13:43:22 +0200

    Add code copy button

--- src/app/resume/timeline.tsx
+++ src/app/resume/timeline.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { JSX, useState } from "react";
+import React, { JSX, useState } from 'react';
 
 export default function Timeline(): JSX.Element {
     const [ expanded, setExpanded ] = useState(false);
